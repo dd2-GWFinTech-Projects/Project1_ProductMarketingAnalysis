65d3cf1e774d879cb10dff05b9f9515be21d092c
David G <davidrgayman@gmail.com>
2020-11-11 11:34:35 -0500

    Partial hacking solution for date parsing...this is the wrong way.

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
+# Install date parsing library 'dateparser'
+conda activate pyvizenv
+pip install dateparser
+
+# Install toolchain to run Jupyter notebook from command line
 sudo pip install -U mistune
-
-# Run the Jupyter notebook
+## Run the Jupyter notebook
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to python nb.ipynb
-
-# Generate HTML output
+## Generate HTML output
 jupyter-nbconvert --execute ""
