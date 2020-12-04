8541e401f4b8e4561fb1201f3169e29a1cffbd1c
David G <davidrgayman@gmail.com>
2020-12-04 00:10:17 -0500

    Dropdown renders, untested.

--- lib2/Constants.py
+++ lib2/Constants.py
@@ -65,3 +65,6 @@ class Constants:
         self.PREPROCESSED_COMBINED_DATA_FILE_PATH         = os.path.join(self.DATA_DIR_PREPROCESSED, Path("CombinedData.pkl"))
 
         # Processed data files
+
+        
+        self.DROP_DOWNS_STATE_ABBREVIATION_LIST = [ "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "DC" ]
