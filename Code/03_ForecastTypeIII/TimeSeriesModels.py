8923bb69486339f8c126d48560549b1cf7d971a6
David G <davidrgayman@gmail.com>
2020-12-08 15:59:19 -0500

    Debugging.

--- Code/03_ForecastTypeIII/TimeSeriesModels.py
+++ Code/03_ForecastTypeIII/TimeSeriesModels.py
@@ -1,7 +1,6 @@
 from sklearn.linear_model import LinearRegression
-import numpy
-from statsmodels.tsa.arima_model import ARMA
 from sklearn.metrics import r2_score
+from statsmodels.tsa.arima_model import ARMA
 import enum
 
 
@@ -16,7 +15,7 @@ class TimeSeriesModelUtilities:
     def __init__(self, debug_level=0):
         self.debug_level = debug_level
 
-    def convert_df_to_dict(data_df, dict_lookup_list):
+    def convert_df_to_dict(self, data_df, dict_lookup_list):
         data_dict = {}
         for dict_lookup in dict_lookup_list:
             data_dict[dict_lookup] = data_df[dict_lookup].values
@@ -213,23 +212,33 @@ class TimeSeriesModelPredictionPreviewUtilities:
 
     def __init__(self, debug_level=0):
         self.debug_level = debug_level
+        self.time_series_model_utilities = TimeSeriesModelUtilities()
     
-    def generate_prediction_preview(self, model_type, values_dict_df, change_values_dict_df):
-        ## Convert data types
-        values_dict=convert_df_to_dict(values_dict_df, classification_list)
-        change_values_dict=convert_df_to_dict(change_values_dict_df, classification_list)
-        ## Build, train, predict
+
+
+
+    def generate_prediction_preview(self, model_type, values_dict_df, change_values_dict_df, dict_lookup_list):
+        
+        # Convert data types
+        values_dict = self.time_series_model_utilities.convert_df_to_dict(values_dict_df, dict_lookup_list)
+        change_values_dict = self.time_series_model_utilities.convert_df_to_dict(change_values_dict_df, dict_lookup_list)
+        dict_lookup_list_prediction_names = [(lambda x: "Predicted_" + str(x))(x) for x in dict_lookup_list]
+        
+        # Build, train, predict
         model__values_dict = self.build_model(model_type, values_dict, change_values_dict)
         model__values_dict.train()
         model_predictions__values_dict = model__values_dict.predict(prediction_year_list)
-        ## Tabulate
+        
+        # Tabulate
         model_predictions__values_dict__df = pd.DataFrame(model_predictions__values_dict, index=prediction_year_list)
-        model_predictions__values_dict__df.columns = classification_list_prediction_names
+        model_predictions__values_dict__df.columns = dict_lookup_list_prediction_names
         display(model_predictions__values_dict__df)
-        ## Display accuracy metrics
+        
+        # Display accuracy metrics
         display(model__values_dict.get_accuracy_metrics())
         display(model__values_dict.print_accuracy_metrics())
-        ## Plot
+        
+        # Plot
         merged_predictions_df__values_dict = pd.concat([values_dict_df, model_predictions__values_dict__df], axis="columns", join="outer")
         display(merged_predictions_df__values_dict)
         return tools.tool_plot_building.generate_plot__hvplot_line(merged_predictions_df__values_dict,
@@ -240,21 +249,21 @@ class TimeSeriesModelPredictionPreviewUtilities:
         if model_type == ModelType.LinearRegressionDictModel:
             return LinearRegressionDictModel(
                 debug_level=debug_level,
-                x=year_list, dict_lookup_list=classification_list,
+                x=year_list, dict_lookup_list=dict_lookup_list,
                 values_dict=values_dict,
                 change_values_dict=change_values_dict,
                 use_multi_stage=False)
         elif model_type == ModelType.PolynomialRegressionDictModel:
             return PolynomialRegressionDictModel(
                 debug_level=debug_level,
-                x=year_list, dict_lookup_list=classification_list,
+                x=year_list, dict_lookup_list=dict_lookup_list,
                 values_dict=values_dict,
                 change_values_dict=change_values_dict,
                 polynomial_degree=3)
         elif model_type == ModelType.ARMARegressionDictModel:
             return ARMARegressionDictModel(
                 debug_level=debug_level,
-                x=year_list, dict_lookup_list=classification_list,
+                x=year_list, dict_lookup_list=dict_lookup_list,
                 values_dict=values_dict,
                 change_values_dict=change_values_dict,
                 order=(1,1))
